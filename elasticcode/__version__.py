08310e0fb577795560c5fdf9891bd4303922a530
Darren Govoni <darren@rapidquest.ai>
2022-01-05 10:02:59 -0500

    Initial setup

---
+++ elasticcode/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'elasticcode'
+__description__ = 'An elastic, distributed python compute platform'
+__version__ = '0.0.1'
+__author__ = 'Darren Govoni'
+__author_email__ = 'darren@ontrenet.com'
+__license__ = 'MIT'
+__url__ = ''
