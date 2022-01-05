22648d044a9233e0dc1bbe18d25d2b4230c0304b
Darren Govoni <darren@rapidquest.ai>
2022-01-05 10:49:28 -0500

    Added repo url

--- elasticcode/__version__.py
+++ elasticcode/__version__.py
@@ -7,8 +7,8 @@ Information about the current version of the py-package-template package.
 
 __title__ = 'elasticcode'
 __description__ = 'An elastic, distributed python compute platform'
-__version__ = '0.0.1'
+__version__ = '0.0.8'
 __author__ = 'Darren Govoni'
 __author_email__ = 'darren@ontrenet.com'
 __license__ = 'MIT'
-__url__ = ''
+__url__ = 'https://github.com/radiantone/elasticcode'
